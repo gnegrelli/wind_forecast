b108da5cfde77871b1953baa76d70daaa28f2ab6
Gabriel Negrelli <gnegrelli13@gmail.com>
2026-04-29 22:03:05 -0400

    Add early stopping conditions to training

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
-import pickle
+import torch
 
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from torch import nn, tensor, no_grad
+from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import trange
 
 
 class ShallowLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, scaler=None):
         super(ShallowLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
         self.linear = nn.Linear(hidden_size, output_size)
+        self.scaler = scaler
 
     def forward(self, x):
         x, _ = self.lstm(x)
         x = self.linear(x)
         return x
+
+    def predict(self, x):
+        x = self.scaler.transform(x)
+        x = self.forward(x)
+        return self.scaler.inverse_transform(x)
+
+class EarlyStopping:
+    def __init__(self, patience: int = 5, delta: float = 1e-3):
+        self.patience = patience
+        self.delta = delta
+        self.best_score = np.inf
+        self.early_stop = False
+        self.counter = 0
+        self.best_model_state = None
+
+    def __call__(self, val_loss, model, model_path: str = None):
+        if val_loss < self.best_score - self.delta:
+            self.best_score = val_loss
+            self.best_model_state = model.state_dict()
+            self.counter = 0
+            if model_path is not None:
+                torch.save(model.state_dict(), model_path)
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+
+
+    def load_best_model(self, model):
+        model.load_state_dict(self.best_model_state)
+
 
 def build_data(data: pd.DataFrame, lookback=1, lookahead=1):
     feature_columns = ['ws_x', 'ws_y', 'ti', 'rho']
@@ -31,73 +63,73 @@ def build_data(data: pd.DataFrame, lookback=1, lookahead=1):
         features.append(data.iloc[i - lookback:i][feature_columns].T.values.flatten().tolist())
         targets.append(data.iloc[i:i + lookahead][target_columns].T.values.flatten().tolist())
 
-    return tensor(features, requires_grad=True), tensor(targets)
+    return torch.tensor(features, requires_grad=True), torch.tensor(targets)
 
 def main():
-   # Load data
-   print("Loading data...")
-   with  open('data/Horns_rev1.pkl', 'rb') as file:
-     data = pickle.load(file)
-     data = data[int(-1 * 365 * 24 * 6):]
+    # Load data
+    print("Loading data...")
+    data = pd.read_pickle('data/Horns_rev1.pkl')
 
-   # Preprocess
-   print("Preprocessing data...")
-   data['ws_x'] = data.apply(lambda x: x.ws * np.cos(np.pi * x.wd / 180), axis=1)
-   data['ws_y'] = data.apply(lambda x: x.ws * np.sin(np.pi * x.wd / 180), axis=1)
+    # Preprocess
+    print("Preprocessing data...")
+    data['ws_x'] = data.apply(lambda x: x.ws * np.cos(np.pi * x.wd / 180), axis=1)
+    data['ws_y'] = data.apply(lambda x: x.ws * np.sin(np.pi * x.wd / 180), axis=1)
 
-   # Split into train-validation-test
-   print("Splitting data...")
-   train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False)
+    # Split into train-validation-test
+    print("Splitting data...")
+    samples_in_year = 365 * 24 * 6
+    train_data = data.iloc[:-samples_in_year]
+    test_data = data.iloc[-samples_in_year:]
+    train_data, val_data = train_test_split(train_data, test_size=0.2, shuffle=False)
 
-   # Scale features
-   print("Scaling data...")
-   scaler = MinMaxScaler(feature_range=(0, 1))
-   scaler.fit(train_data)
+    # Scale features
+    print("Scaling data...")
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaler.fit(train_data)
 
-   train_dataset = pd.DataFrame(scaler.transform(train_data), columns=train_data.columns)
-   test_dataset = pd.DataFrame(scaler.transform(test_data), columns=test_data.columns)
+    train_dataset = pd.DataFrame(scaler.transform(train_data), columns=train_data.columns)
+    val_dataset = pd.DataFrame(scaler.transform(val_data), columns=val_data.columns)
 
-   print("Building datasets...")
-   x_train, y_train = build_data(train_dataset, lookback=6 * 24 * 5, lookahead=6 * 24 * 2)
-   x_test, y_test = build_data(test_dataset, lookback=6 * 24 * 5, lookahead=6 * 24 * 2)
+    print("Building datasets...")
+    x_train, y_train = build_data(train_dataset, lookback=6 * 24 * 5, lookahead=6 * 24 * 2)
+    x_val, y_val = build_data(val_dataset, lookback=6 * 24 * 5, lookahead=6 * 24 * 2)
 
-   train_dataloader = DataLoader(TensorDataset(x_train, y_train), batch_size=2048, shuffle=True)
+    train_dataloader = DataLoader(TensorDataset(x_train, y_train), batch_size=4096, shuffle=True)
 
-   # Define model, optimizer and loss function
-   print("Defining model...")
-   model = ShallowLSTM(6 * 24 * 5 * 4, 6 * 24 * 5, 6 * 24 * 2 * 2)
-   optimizer = Adam(model.parameters())
-   loss_function = nn.MSELoss()
-   previous_loss = np.inf
-   
-   # Run training
-   n_epochs = 100
-   print(f"Training model for {n_epochs} epochs...")
-   for epoch in trange(n_epochs):
-       print(f" - Starting epoch #{epoch}")
-       model.train()
-       for x, y in train_dataloader:
-           y_hat = model.forward(x)
-           loss = loss_function(y_hat, y)
-           optimizer.zero_grad()
-           loss.backward()
-           optimizer.step()
-           print(f" - Epoch #{epoch}: batch MSE {loss:.4f}")
+    # Define model, optimizer and loss function
+    print("Defining model...")
+    model = ShallowLSTM(6 * 24 * 5 * 4, 6 * 24 * 5, 6 * 24 * 2 * 2, scaler=scaler)
+    optimizer = Adam(model.parameters())
+    loss_function = nn.MSELoss()
+    early_stopping = EarlyStopping(patience=5, delta=0.001)
 
-       model.eval()
-       with no_grad():
-           y_hat = model.forward(x_train)
-           train_loss = loss_function(y_hat, y_train)
-           y_hat = model.forward(x_test)
-           test_loss = loss_function(y_hat, y_test)
-       print(f"Epoch #{epoch}: train MSE {train_loss:.4f}, test MSE {test_loss:.4f}")
-       if test_loss < previous_loss:
-           print("Saving model...")
-           previous_loss = test_loss
-           with open(f'models/ShallowLSTM_epoch{epoch}_loss{test_loss:.4f}.pkl', 'wb') as file:
-               pickle.dump(model.state_dict(), file)
+    # Run training
+    n_epochs = 100
+    print(f"Training model for {n_epochs} epochs...")
+    for epoch in trange(n_epochs):
+        print(f" - Starting epoch #{epoch}")
+        model.train()
+        for x, y in train_dataloader:
+            y_hat = model.forward(x)
+            loss = loss_function(y_hat, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print(f" - Epoch #{epoch}: batch MSE {loss:.4f}")
+
+        model.eval()
+        with torch.no_grad():
+            y_hat = model.forward(x_train)
+            train_loss = loss_function(y_hat, y_train)
+            y_hat = model.forward(x_val)
+            val_loss = loss_function(y_hat, y_val)
+        print(f"Epoch #{epoch}: train MSE {train_loss:.4f}, validation MSE {val_loss:.4f}")
+
+        early_stopping(val_loss, model, model_path=f'models/ShallowLSTM_epoch{epoch}_vloss{val_loss:.4f}.pkl')
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
 
 
 if __name__ == "__main__":
-   main()
- 
+    main()
